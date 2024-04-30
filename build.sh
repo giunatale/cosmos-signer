c65aa559d8f0c4ba7628da6c02745d6e46b5136c
Giuseppe Natale <12249307+giunatale@users.noreply.github.com>
2024-04-30 10:51:36 +0200

    slightly improve build messages

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-echo "building cosmos-signer"
+mkdir -p build
+echo "Building cosmos-signer"
 go build -C cmd/cosmos-signer -o ../../build/cosmos-signer
 pushd plugins > /dev/null
 ./build.sh
--- plugins/build.sh
+++ plugins/build.sh
@@ -3,10 +3,10 @@
 for dir in */; do
     if [[ -f "${dir}build.sh" ]]; then
         pushd "${dir}" > /dev/null
-        echo "Running build.sh in ${dir%/}"
+        echo "Building ${dir%/} plugin"
         ./build.sh
-    mkdir -p "../../build/plugins"
-	mv plugin.so "../../build/plugins/${dir%/}.so"
+        mkdir -p "../../build/plugins"
+        mv plugin.so "../../build/plugins/${dir%/}.so"
         popd > /dev/null
     else
         echo "No build.sh found in ${dir%/}"
